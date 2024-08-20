32f60de8a483d65591b7e6d85e366da84396ebde
Леонид Иванов <ivanov.l@robotkarl.com>
2024-08-20 23:42:24 +0800

    add link

--- src/components/core/CreatedAt.js
+++ src/components/core/CreatedAt.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function CreatedAt(props) {
   return (
-    <div className={"flex items-end gap-[15px]"}>
+    <a href={'https://offthegridlab.com/'} className={"flex link items-end gap-[15px]"}>
       <span className={"text-[18px]"}>Design by</span>
       <img src="/OffTheDrid.png" className={"max-w-[137px] mb-[3px]"} alt="Дизайн Лого"/>
-    </div>
+    </a>
   );
 }
 
