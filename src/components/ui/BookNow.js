076d2ac364bf411aa693a741d2064a87c5ae7aa7
Леонид Иванов <ivanov.l@robotkarl.com>
2024-08-22 04:22:14 +0800

    fixed skipper

--- src/components/ui/BookNow.js
+++ src/components/ui/BookNow.js
@@ -3,30 +3,9 @@ import Button from "./Button";
 
 function BookNow({color, hoverColor}) {
   return (
-    <div>
-      <a data-testid="linkElement" href="#" target="_self" className="skipper-be-cta">
-        <Button label={'Book Now'} color={color} hoverColor={hoverColor}/>
-      </a>
-      <hr/>
-      <div>
-        <button className="primary-btn skipper-be-cta">Book Now</button>
-      </div>
-      <hr/>
-      <div>
-        <a data-testid="linkElement" href="https://www.irishotelboston.com/rooms" target="_self"
-           className="Y3Mib0">Rooms</a>
-      </div>
-      <hr/>
-      <div>
-        <a data-testid="linkElement" href="?sbe_internalLink" target="_self" className="Y3Mib0">Rooms</a>
-      </div>
-      <hr/>
-      <div>
-        <a data-testid="linkElement" href="#" target="_self" className="skipper-be-cta">Rooms</a>
-      </div>
-    </div>
-
-
+    <a data-testid="linkElement" href="?sbe_internalLink" target="_self" className="Y3Mib0">
+      <Button label={'Book Now'} color={color} hoverColor={hoverColor}/>
+    </a>
   );
 }
 
