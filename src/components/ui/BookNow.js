0024f6bcba64c8e4748b649db1400a44a2f699cb
Леонид Иванов <ivanov.l@robotkarl.com>
2024-08-22 03:54:31 +0800

    asd

--- src/components/ui/BookNow.js
+++ src/components/ui/BookNow.js
@@ -3,9 +3,30 @@ import Button from "./Button";
 
 function BookNow({color, hoverColor}) {
   return (
-    <a data-testid="linkElement" href="#" target="_self" className="skipper-be-cta">
-      <Button label={'Book Now'} color={color} hoverColor={hoverColor}/>
-    </a>
+    <div>
+      <a data-testid="linkElement" href="#" target="_self" className="skipper-be-cta">
+        <Button label={'Book Now'} color={color} hoverColor={hoverColor}/>
+      </a>
+      <hr/>
+      <div>
+        <button className="primary-btn skipper-be-cta">Book Now</button>
+      </div>
+      <hr/>
+      <div>
+        <a data-testid="linkElement" href="https://www.irishotelboston.com/rooms" target="_self"
+           className="Y3Mib0">Rooms</a>
+      </div>
+      <hr/>
+      <div>
+        <a data-testid="linkElement" href="?sbe_internalLink" target="_self" className="Y3Mib0">Rooms</a>
+      </div>
+      <hr/>
+      <div>
+        <a data-testid="linkElement" href="#" target="_self" className="skipper-be-cta">Rooms</a>
+      </div>
+    </div>
+
+
   );
 }
 
