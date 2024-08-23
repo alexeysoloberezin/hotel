4507590a05d7bf1af06a797342f3a109a507bad1
Leonid Ivanov <71139559+alexeysoloberezin@users.noreply.github.com>
2024-08-23 14:02:57 +0800

    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,11 +19,11 @@ const useHash = () => {
 function Home(props) {
   const hash = useHash()
   const [isRender1, ref1] = useIsRender("0px 0px");
-  const [isRender2, ref2] = useIsRender();
-  const [isRender3, ref3] = useIsRender();
-  const [isRender4, ref4] = useIsRender();
-  const [isRender5, ref5] = useIsRender();
-  const [isRender6, ref6] = useIsRender();
+  const [isRender2, ref2] = useIsRender("20% 0px");
+  const [isRender3, ref3] = useIsRender("20% 0px");
+  const [isRender4, ref4] = useIsRender("20% 0px");
+  const [isRender5, ref5] = useIsRender("20% 0px");
+  const [isRender6, ref6] = useIsRender("20% 0px");
 
   useEffect(() => {
     if (hash) {
@@ -39,52 +39,45 @@ function Home(props) {
     <div className={clsx(' relative  flex flex-col')}>
       <MainBlock/>
 
-      {isRender1 && (
-        <div ref={ref1}>
+      <div ref={ref1} style={{minHeight:' 400px'}}>
+        {isRender1 && (
           <Accommodation/>
-        </div>
-      )}
+        )}
+      </div>
       
-
-      
-      {isRender2 && (
-        <div ref={ref2}>
+      <div ref={ref2} style={{minHeight:' 400px'}}>
+        {isRender2 && (
           <AboutUs/>
-        </div>
-      )}
+        )}
+      </div>
 
-      {isRender3 && (
-        <div ref={ref3}>
-           <BgImageBlock bg={'/bg1.jpg'} href={'/restaurant'} topImg={'/petalLogo.png'}>
-              <h1  data-aos="fade-up" data-aos-delay="0">Petal Restaurant</h1>
-              <p  data-aos="fade-up" data-aos-delay="200" className={"max-w-[578px] mt-[20px]"}>Indulge in breakfast or dinner in our elegantly adorned restaurant, featuring a selection of healthy cuisine,
-                all while marveling at the ceiling painted by the renowned Italian artist, Giovanni De Cunto. </p>
-            </BgImageBlock>
-        </div>
-      )}
-     
-      {isRender4 && (
-        <div ref={ref4}>
-            <Bar />
-        </div>
-      )}
-    
-      {isRender5 && (
-        <div ref={ref5}>
-            <Spa />
-        </div>
-      )}
+      <div ref={ref3} style={{minHeight:' 400px'}}>
+        {isRender3 && (
+         <BgImageBlock bg={'/bg1.jpg'} href={'/restaurant'} topImg={'/petalLogo.png'}>
+         <h1  data-aos="fade-up" data-aos-delay="0">Petal Restaurant</h1>
+         <p  data-aos="fade-up" data-aos-delay="200" className={"max-w-[578px] mt-[20px]"}>Indulge in breakfast or dinner in our elegantly adorned restaurant, featuring a selection of healthy cuisine,
+           all while marveling at the ceiling painted by the renowned Italian artist, Giovanni De Cunto. </p>
+         </BgImageBlock>
+        )}
+      </div>
 
-      {isRender6 && (
-        <div ref={ref6}>
-           <Contacts />
-        </div>
-      )}
+      <div ref={ref4} style={{minHeight:' 400px'}}>
+        {isRender4 && (
+         <Bar />
+        )}
+      </div>
 
-    
+      <div ref={ref5} style={{minHeight:' 400px'}}>
+        {isRender5 && (
+        <Spa />
+        )}
+      </div>
 
-      
- 
+      <div ref={ref6} style={{minHeight:' 400px'}}>
+        {isRender6 && (
+        <Contacts />
+        )}
+      </div>
     </div>
   );
 }
