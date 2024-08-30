261f52a6a060a6c9f51877c8f01dd07baa230ec7
Леонид Иванов <ivanov.l@robotkarl.com>
2024-08-30 22:43:07 +0800

    s

--- src/components/core/PerfectGrid.js
+++ src/components/core/PerfectGrid.js
@@ -8,7 +8,7 @@ function PerfectGrid({imgTemplate, hideMob}) {
 
   const indexClasses = [
     'order-1 md:order-1',
-    'order-3 md:order-2 row-span-2 col-span-2 md:col-span-1 md:max-h-fit max-h-[345px]',
+    'order-3 md:order-2 row-span-2 col-span-2 md:col-span-1 md:max-h-fit h-[580px] max-h-[345px]',
     'order-4 md:order-3',
     'order-2 md:order-4',
     'order-5 md:order-5',
@@ -21,13 +21,13 @@ function PerfectGrid({imgTemplate, hideMob}) {
       })
     }>
       {grid.map((el, index) => (
-        <Animation delay={index * .2 + 's'} className={indexClasses[index]}>
+        <div className={indexClasses[index]}>
           <Image
             noWebp={true}
+            animationClass={index === 1 ? 'h-[596px]' : ''}
             className={clsx("w-full h-full object-cover",
           )} src={imgTemplate.replace('{**}', index + 1)} alt="bar-img"/>
-        </Animation>
-        
+        </div>
       ))}
     </div>
   );
