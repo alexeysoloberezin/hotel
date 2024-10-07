ece812ec4cbf30e7822ca4615bbf80267264a6ee
Леонид Иванов <alexeysoloberezinsolo@gmail.com>
2024-10-07 20:04:13 +0800

    zxcsd

--- src/components/core/MainBlockTemplate.js
+++ src/components/core/MainBlockTemplate.js
@@ -1,11 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Header from "./Header";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import Animation from '../Animation';
 
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
+};
+
 function MainBlockTemplate({video,videoWebM,preview, image,mobileGif, title, description, children}) {
   const isMobile = useMediaQuery(false, '(max-width: 600px)');
 
+  const videoParentRef = useRef(null);
+
+  useEffect(() => {
+    // проверяем, что это Safari и что есть ссылка на родительский элемент <div />
+    if (isSafari() && videoParentRef.current) {
+      // получаем ссылку на элемент <video>
+      const player = videoParentRef.current.children[0];
+
+      // если ссылка на видео получена
+      if (player) {
+        // устанавливаем атрибуты видео через JS (политика WebKit)
+        player.controls = false;
+        player.playsInline = true;
+        player.muted = true;
+        player.setAttribute("muted", ""); // все возможные варианты
+        player.autoplay = true;
+
+        // используем setTimeout для асинхронного вызова
+        setTimeout(() => {
+          // player.play() может вернуть promise
+          const promise = player.play();
+          if (promise && promise.then) {
+            promise
+              .then(() => {
+                // видео воспроизводится успешно
+              })
+              .catch(() => {
+                // если воспроизведение не удалось, скрываем видео и показываем альтернативу
+                videoParentRef.current.style.display = "none";
+              });
+          }
+        }, 0);
+      }
+    }
+  }, [isSafari]);
 
 
   return (
@@ -19,17 +59,36 @@ function MainBlockTemplate({video,videoWebM,preview, image,mobileGif, title, des
               className={`absolute top-0 left-1/2 translate-x-[-50%] w-fit max-w-fit h-full`}
             />
           ) : video ? (
-            <video
-              autoPlay
-              preload="auto"
-              loop
-              muted={true}
-              playsInline={true}
-              className={`absolute top-0 left-0 w-full h-full object-cover`}
-            >
-              <source src={videoWebM} type="video/webm" />
-              <source src={video} type="video/mp4" />
-            </video>
+            // <video
+            //   autoPlay
+            //   preload="auto"
+            //   loop
+            //   muted={true}
+            //   playsInline={true}
+            //   className={`absolute top-0 left-0 w-full h-full object-cover`}
+            // >
+            //   <source src={videoWebM} type="video/webm" />
+            //   <source src={video} type="video/mp4" />
+            // </video>
+            <div
+              ref={videoParentRef}
+              dangerouslySetInnerHTML={{
+                __html: `
+                <video
+                  loop
+                  muted
+                  autoplay
+                  playsinline
+                  preload="metadata"
+                  class="absolute top-0 left-0 w-full h-full object-cover"
+                  width="100%"
+                  >
+                  <source src=${videoWebM} type="video/webm" />
+                  <source src="${video}" type="video/mp4" />
+                <track kind="caption" />
+                </video>`
+                      }}
+                />
           ) : (
             <img
               src={image}
