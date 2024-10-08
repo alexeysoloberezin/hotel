f2bba2b5f5b26379452c681330b9fbe2e3903832
Леонид Иванов <alexeysoloberezinsolo@gmail.com>
2024-10-09 01:09:50 +0800

    fix video

--- src/components/core/MainBlockTemplate.js
+++ src/components/core/MainBlockTemplate.js
@@ -2,78 +2,34 @@ import React, {useEffect, useRef, useState} from 'react';
 import Header from "./Header";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import Animation from '../Animation';
-import ReactPlayer from 'react-player'
 
-const isSafari = () => {
-  const ua = navigator.userAgent.toLowerCase();
-  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
-};
 
 function MainBlockTemplate({video,videoWebM,preview, image,mobileGif, title, description, children}) {
   const isMobile = useMediaQuery(false, '(max-width: 600px)');
-  const [errorLoadVideo, setErrorLoadVideo] = useState(false);
-
-  const videoParentRef = useRef(null);
-
-  useEffect(() => {
-    // проверяем, что это Safari и что есть ссылка на родительский элемент <div />
-    if (isSafari() && videoParentRef.current) {
-      // получаем ссылку на элемент <video>
-      const player = videoParentRef.current.children[0];
-
-      if (player) {
-        player.controls = false;
-        player.playsInline = true;
-        player.muted = true;
-        player.setAttribute("muted", ""); // все возможные варианты
-        player.autoplay = true;
-
-        setTimeout(() => {
-          const promise = player.play();
-          if (promise && promise.then) {
-            promise
-              .then(() => {
-              })
-              .catch(() => {
-                  setErrorLoadVideo(true)
-              });
-          }
-        }, 0);
-      }
-    }
-  }, [isSafari]);
 
 
   return (
     <div>
       <div className={"h-screen relative flex flex-col justify-center items-center pt-[80px] pb-[80px]"}>
         <div>
-          {errorLoadVideo && isMobile ? (
+          {isMobile && mobileGif ? (
             <img
               src={mobileGif}
               alt=""
               className={`absolute top-0 left-1/2 translate-x-[-50%] w-fit max-w-fit h-full`}
             />
           ) : video ? (
-            <div
-              ref={videoParentRef}
-              dangerouslySetInnerHTML={{
-                __html: `
-                <video
-                  loop
-                  muted
-                  autoplay
-                  playsinline
-                  preload="metadata"
-                  class="absolute top-0 left-0 w-full h-full object-cover"
-                  width="100%"
-                  >
-<!--                  <source src=${videoWebM} type="video/webm" />-->
-                  <source src="${video}" type="video/mp4" />
-                    <track kind="caption" />
-                </video>`
-                      }}
-                />
+            <video
+              autoPlay
+              preload="auto"
+              loop
+              muted={true}
+              playsInline={true}
+              className={`absolute top-0 left-0 w-full h-full object-cover`}
+            >
+              <source src={videoWebM} type="video/webm" />
+              <source src={video} type="video/mp4" />
+            </video>
           ) : (
             <img
               src={image}
